9e6089fa5ce696fa3649a16adab86a319be697ea
sarmatics <grybuk@gmail.com>
2017-03-13 22:48:54 +0200

    Replace not valid IPs

--- backup.sh
+++ backup.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-/usr/bin/mongodump --host "testlast/52.59.244.253:27017,54.93.74.111:27017,52.59.231.7:27017" --out /backup/$(date +%Y-%m-%dT%H-%M)_testlast
+/usr/bin/mongodump --host "testlast/10.42.241.251:27017,10.42.73.149:27017,10.42.214.14:27017" --out /backup/$(date +%Y-%m-%dT%H-%M)_testlast
 
 find /backup/  -mmin +301 -type d  -exec rm -rf {} \;  
 
